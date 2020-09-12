8e6c667dc79387fbc69de56b7f699d00b2a92d71
HdroguettA <48663306+HdroguettA@users.noreply.github.com>
2020-09-12 21:21:12 +1000

    Initial Files/Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, withRouter  } from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="webpage-content">
+      <SideNav />
+        <Switch>
+          <Route exact path="/" component={withRouter(Search)}>
+          </Route>
+        </Switch>
+        </div>
+    </Router>
     </div>
   );
 }
-
-export default App;
