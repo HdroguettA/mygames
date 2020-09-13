ba92f5dd4f5dc3f02e7e3dd2111773e669a7f76f
HdroguettA <48663306+HdroguettA@users.noreply.github.com>
2020-09-13 20:28:36 +1000

    First Commit to search branch. Reorganised contents of certain files to make logical sense

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component }  from 'react';
+import './Search.css';
+import GamesUnderline from "./svg/GamesUnderline.svg";
+
+function Home() {
+  return (
+    <div className="home-main-content">
+      <div className="game-heading">
+        <p className="game-heading-txt">
+          Games
+        </p>
+        <img src={GamesUnderline} alt="Games text underline" className="game-heading-underline"/>
+      </div>
+      <div className="search-bar">
+      </div>
+    </div>
+  );
+}
+
+export default Home;
