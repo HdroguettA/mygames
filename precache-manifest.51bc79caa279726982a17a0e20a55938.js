d1db2c8b3aee2f280f2bfc96a918f0de1d6d02ce
HdroguettA <48663306+HdroguettA@users.noreply.github.com>
2020-09-15 20:31:37 +1000

    Updates

---
+++ precache-manifest.51bc79caa279726982a17a0e20a55938.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b968afc96ab0281b44c6ee0b5bcfa8c",
+    "url": "/mygames/index.html"
+  },
+  {
+    "revision": "e78813466b6a225e624f",
+    "url": "/mygames/static/css/main.d1af5dc0.chunk.css"
+  },
+  {
+    "revision": "c5f5d6313ca6bcee1be9",
+    "url": "/mygames/static/js/2.827a5172.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mygames/static/js/2.827a5172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78813466b6a225e624f",
+    "url": "/mygames/static/js/main.2262ed1c.chunk.js"
+  },
+  {
+    "revision": "e692ee266a4ec084e4d7",
+    "url": "/mygames/static/js/runtime-main.a9ad908b.js"
+  },
+  {
+    "revision": "e4ec94363e622f7a8cf4ec98bb4421eb",
+    "url": "/mygames/static/media/GamesUnderline.e4ec9436.svg"
+  }
+]);
