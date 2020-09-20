a49a749dc464fdb41071856dd221c2f5a1bc8009
HdroguettA <48663306+HdroguettA@users.noreply.github.com>
2020-09-20 15:53:06 +1000

    Added autosizing input field + small style changes to it

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,34 @@
 import React, { Component }  from 'react';
 import './Search.css';
+import AutosizeInput from 'react-input-autosize';
 
-function Search() {
+class Search extends Component {
+  constructor (props) {
+		super();
+		this.state = {
+			searchValue: '',
+		};
+	}
+	updateInputValue = (input, event) => {
+		const newState = {};
+		newState[input] = event.target.value;
+    this.setState(newState);
+    document.getElementById("caret").style.margin = 0;
+    document.getElementById("caret").style.animation = 0;
+  };
+  render () {
   return (
     <div className="search-main-content">
-      <input></input>
-        <span className="caret">|</span>
+      <AutosizeInput
+          
+					autoFocus
+					value={this.state.searchValue}
+          onChange={this.updateInputValue.bind(this, 'searchValue')}
+          className="search-input"
+				/>
+        <span id="caret" className="caret">|</span>
     </div>
   );
 }
-
+}
 export default Search;
