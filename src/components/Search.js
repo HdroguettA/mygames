9627604020235a6a50942f1902ab4ede51ef3fa3
HdroguettA <48663306+HdroguettA@users.noreply.github.com>
2020-09-22 22:49:47 +1000

    Added autocomplete as well as search bar UI responsiveness

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,35 +1,44 @@
 import React, { Component }  from 'react';
 import './Search.css';
 import AutosizeInput from 'react-input-autosize';
+import autocomplete from 'autocompleter';
+
+
 
 class Search extends Component {
+  
   constructor (props) {
 		super();
 		this.state = {
 			searchValue: '',
-		};
-	}
-	updateInputValue = (input, event) => {
-    document.getElementById("caret").style.animation = 0;
-		const newState = {};
-		newState[input] = event.target.value;
-    this.setState(newState);
-    document.getElementById("caret").style.margin = 0;
-      document.getElementById("caret").style.animation = "1.6s blink linear infinite";
+    };
     
-  };
-
+  }
+  componentDidMount () {
+    let countries = [
+      { label: 'United Kingdom', value: 'UK' },
+      { label: 'United States', value: 'US' }
+    ];
+    let input = document.getElementById("games");
+    autocomplete({
+      input: input,
+      fetch: function(text, update) {
+          text = text.toLowerCase();
+          // you can also use AJAX requests instead of preloaded data
+          var suggestions = countries.filter(n => n.label.toLowerCase().startsWith(text))
+          update(suggestions);
+      },
+      onSelect: function(item) {
+          input.value = item.label;
+      }
+  })
+  }
+  //<span id="caret" className="caret">|</span>
   render () {
   return (
     <div className="search-main-content">
-      <AutosizeInput
-					autoFocus
-          value={this.state.searchValue}
-          style={{ fontSize: 36 }}
-          onChange={this.updateInputValue.bind(this, 'searchValue')}
-          className="search-input"
-				/>
-        <span id="caret" className="caret">|</span>
+      <input id="games" type="text" spellcheck="false" className="search-input"></input>
+
     </div>
   );
 }
