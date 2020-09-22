e4b9f45e966a4ce8a8e869994adf7269ab8e7bec
HdroguettA <48663306+HdroguettA@users.noreply.github.com>
2020-09-22 23:31:04 +1000

    Autocomplete styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,8 +16,10 @@ class Search extends Component {
   }
   componentDidMount () {
     let countries = [
-      { label: 'United Kingdom', value: 'UK' },
-      { label: 'United States', value: 'US' }
+      { label: 'Pong', value: 'UK' },
+      { label: 'Asteroids', value: 'US' },
+      { label: 'Asteroidsasd', value: 'US' },
+      { label: 'Asteroidsaa', value: 'US' }
     ];
     let input = document.getElementById("games");
     autocomplete({
@@ -27,6 +29,12 @@ class Search extends Component {
           // you can also use AJAX requests instead of preloaded data
           var suggestions = countries.filter(n => n.label.toLowerCase().startsWith(text))
           update(suggestions);
+          console.log(suggestions[0]);
+          try {
+            document.getElementById("games").value = suggestions[0].label;
+          } catch (e) {
+            console.log("No more suggestions")
+          }
       },
       onSelect: function(item) {
           input.value = item.label;
